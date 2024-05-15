fc216fd689a7cc44107af4b13510af8f699db5ec
daetu01 <daetu01@gmail.com>
2024-05-16 01:39:43 +0900

    add drop query & sequence division_seq

--- SSG_oracle/INSERT_FINAL.sql
+++ SSG_oracle/INSERT_FINAL.sql
@@ -113,6 +113,14 @@ increment by 1
 nocache 
 nocycle;
 
+CREATE sequence division_seq
+start with 1 
+increment by 1 
+nocache 
+nocycle;
+
+
+
 -- 회원insert 프로시저 
 create or replace PROCEDURE ins_member 
 (
