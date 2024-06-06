f44438c6f29558b1c3c87f0980af303319365546
daetu01 <80326475+daetu01@users.noreply.github.com>
2024-06-06 14:24:25 +0900

    Update INSERT_FINAL.sql
    password UPdate daetu0

--- SSG_oracle/INSERT_FINAL.sql
+++ SSG_oracle/INSERT_FINAL.sql
@@ -158,7 +158,7 @@ nocycle;
 
 -- 회원 INSERT
 -- 더미데이터 
-INSERT INTO MEMBER VALUES('daetu01','daetu01@gmail.com','010-1111-1111','원대만','1234','1978-05-29',SYSDATE,SYSDATE,'0','0','N');
+INSERT INTO MEMBER VALUES('daetu01','daetu01@gmail.com','010-1111-1111','원대만','$2a$10$Gp6o1tynZPeQNw2Jg1zAeuDRZpDzxitFyNcrsl6d104WfuEr9BWIO','1978-05-29',SYSDATE,SYSDATE,'0','0','N');
 INSERT INTO MEMBER VALUES('m_eum01','m_eum01@naver.com','010-4142-2134','권맑음','1234','1998-09-12',SYSDATE,SYSDATE,'0','0','N');
 INSERT INTO MEMBER VALUES('d_Chan01','d_Chan01@daum.com','010-1231-2685','이동찬','1234','1995-11-20',SYSDATE,SYSDATE,'0','0','N');
 INSERT INTO MEMBER VALUES('dyoung01','dyoung01@gmail.com','010-1245-5415','이동영','1234','1990-01-02',SYSDATE,SYSDATE,'0','0','N');
